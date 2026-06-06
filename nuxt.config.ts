4f7dddd557b64069cf79098c703b41b2ec4a5361
Zoilo Bravo Salud Jr <zoilo@nmgresources.ph>
2026-06-06 13:05:12 +0800

    Change env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
     public: {
-      apiBase: 'http://127.0.0.1:8000'
+      apiBase: process.env.NUXT_PUBLIC_API_BASE || 'http://127.0.0.1:8000'
     }
   },
   css: ['./app/assets/main.css'],
