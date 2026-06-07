359a28c18393b02771fd6010ff5a188357ed0141
Zoilo Bravo Salud Jr <zoilo@nmgresources.ph>
2026-06-07 13:36:22 +0800

    Updates

--- app/components/Navbar.vue
+++ app/components/Navbar.vue
@@ -6,7 +6,6 @@
                 <li><RouterLink to="/" :class="`${isActivePage('/') ? 'bg-amber-950 text-amber-50' : 'hover:bg-amber-200'} border rounded px-5 py-2`">Projects</RouterLink></li>
                 <li><RouterLink to="/categories" :class="`${isActivePage('/categories') ? 'bg-amber-950 text-amber-50' : 'hover:bg-amber-200'} border rounded px-5 py-2`">Categories</RouterLink></li>
                 <li><RouterLink to="/subcategories" :class="`${isActivePage('/subcategories') ? 'bg-amber-950 text-amber-50' : 'hover:bg-amber-200'} border rounded px-5 py-2`">Sub-categories</RouterLink></li>
-                <li><RouterLink to="/projects" :class="`${isActivePage('/projects') ? 'bg-amber-950 text-amber-50' : 'hover:bg-amber-200'} border rounded px-5 py-2`">Compute Project Costing</RouterLink></li>
             </ul>
         </div>
     </header>
